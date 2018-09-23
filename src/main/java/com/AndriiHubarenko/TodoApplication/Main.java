64bdbc19474d17ee92f48a52c11cc49cce382395
Andrii Gubarenko <Andrewgubarenko@gmail.com>
2018-09-23 19:32:37 +0300

    Was provided a refactoring aimed to improve and to compact the code

--- src/main/java/com/AndriiHubarenko/TodoApplication/Main.java
+++ src/main/java/com/AndriiHubarenko/TodoApplication/Main.java
@@ -1,22 +1,25 @@
 package com.AndriiHubarenko.TodoApplication;
-
 import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 //		TodoBody taskOne = new TodoBody();
-//		taskOne.setName("Task 1");
-//		taskOne.setComment("First test comment of todo");
+//		taskOne.setName("Task 6");
+//		taskOne.setComment("Number 6 test comment of todo");
 //		taskOne.setDeadLine(new Date());
 //		taskOne.setIsFinished(false);
 
 		ITodoService service = new TodoService();
 //		service.create(taskOne);
-		List<TodoBody> list = service.getTodoList();
-		TodoBody updatedTodo = list.get(0);
-		updatedTodo.setComment("NEW UPDATED COMMENT FOR TESTING");
 		
+		List<TodoBody> list = service.getTodoList();
+//		TodoBody updatedTodo = list.get(0);
+//		updatedTodo.setComment("final test today");
+//		service.update(updatedTodo);
+		for(TodoBody l: list) {
+			System.out.println(Converter.fromTodoBodyToJson(l));
+		}
  		System.out.println(service.todoCount());
 	}
 
--- src/main/java/com/AndriiHubarenko/TodoApplication/TodoService.java
+++ src/main/java/com/AndriiHubarenko/TodoApplication/TodoService.java
@@ -1,14 +1,7 @@
 package com.AndriiHubarenko.TodoApplication;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,39 +9,17 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class TodoService implements ITodoService {
-	String tableName = "TodoApplication";
-	String targetURL = "https://api.backendless.com/006F4BC5-000C-0188-FF4C-BAB117143000/0C5FB588-52CA-F8E5-FF82-8E4947283E00/data/" + tableName;
 	HttpURLConnection con = null;
 
 	public TodoBody create(TodoBody body) {
 		try {
-			URL url = new URL(targetURL);
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("POST");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setDoOutput(true);
-			con.setDoInput(true);
+			con = ConnectionUtils.getConnection(true, "", "POST");
+			
+			ConnectionUtils.sendRequest(con, body);
+			System.out.println(ConnectionUtils.getResponse(con));
+			return body;
 
-			// Send request
-			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(wr, "UTF-8"));
-			writer.write(Converter.fromTodoBodyToJson(body).toString());
-			writer.flush();
-			writer.close();
-
-			// Get Response
-			InputStream is = con.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			StringBuffer response = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			rd.close();
-			return Converter.fromJsonToTodoBody(new JSONObject(response));
-
-		} catch (MalformedURLException ex) {
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			return null;
 		} catch (Exception ex) {
@@ -63,28 +34,16 @@ public class TodoService implements ITodoService {
 
 	public TodoBody getTodo(String parameter) {
 		TodoBody result = new TodoBody();
-
+		
 		return result;
 	}
 
 	public int todoCount() {
-		int count = 0;
 		try {
-			URL url = new URL(targetURL + "/count");
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setDoOutput(false);
-			con.setDoInput(true);
+			con = ConnectionUtils.getConnection(false, "/count", "GET");	
+			return Integer.parseInt(ConnectionUtils.getResponse(con).trim());
 
-			// Get Response
-			InputStream is = con.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			count = Integer.parseInt(rd.readLine());
-			rd.close();
-			return count;
-
-		} catch (MalformedURLException ex) {
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			return 0;
 		} catch (Exception ex) {
@@ -99,33 +58,14 @@ public class TodoService implements ITodoService {
 
 	public List<TodoBody> getTodoList() {
 		try {
-			URL url = new URL(targetURL);
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setDoOutput(false);
-			con.setDoInput(true);
-
-			// Get Response
-			InputStream is = con.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			StringBuffer response = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			rd.close();
-			JSONArray jsonResponse = new JSONArray(response.toString());
+			con = ConnectionUtils.getConnection(false, "", "GET");
+			JSONArray jsonResponse = new JSONArray(ConnectionUtils.getResponse(con));
 			List<TodoBody> result = new ArrayList<TodoBody>();
-
 			for (int i = 0; i < jsonResponse.length(); i++) {
 				result.add(Converter.fromJsonToTodoBody((JSONObject) jsonResponse.get(i)));
 			}
-
 			return result;
-
-		} catch (MalformedURLException ex) {
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			return null;
 		} catch (Exception ex) {
@@ -140,33 +80,13 @@ public class TodoService implements ITodoService {
 
 	public TodoBody update(TodoBody body) {
 		try {
-			URL url = new URL(targetURL + "/" + body.getObjectId());
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("PUT");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setDoOutput(true);
-			con.setDoInput(true);
+			con = ConnectionUtils.getConnection(true, "/" + body.getObjectId(), "PUT");
 
-			// Send request
-			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(wr, "UTF-8"));
-			writer.write(Converter.fromTodoBodyToJson(body).toString());
-			writer.flush();
-			writer.close();
-
-			// Get Response
-			InputStream is = con.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			StringBuffer response = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			rd.close();
+			ConnectionUtils.sendRequest(con, body);
+			System.out.println(ConnectionUtils.getResponse(con));
 			return body;
 
-		} catch (MalformedURLException ex) {
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			return null;
 		} catch (Exception ex) {
@@ -181,26 +101,11 @@ public class TodoService implements ITodoService {
 
 	public String remove(String id) {
 		try {
-			URL url = new URL(targetURL + "/" + id);
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("DELETE");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setDoOutput(true);
-			con.setDoInput(true);
+			con = ConnectionUtils.getConnection(false, "/" + id, "DELETE");
 
-			// Get Response
-			InputStream is = con.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			StringBuffer response = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			rd.close();
-			return response.toString();
+			return ConnectionUtils.getResponse(con);
 
-		} catch (MalformedURLException ex) {
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			return null;
 		} catch (Exception ex) {
